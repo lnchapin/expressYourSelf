3e35048e8068ece0dad66751b890ec8dc49e70ce
Lindsay Chapin <lnchapin@gmail.com>
2019-07-17 17:16:22 -0600

    removing extra line

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
